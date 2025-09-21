5dd006aebd6f07b2d85506d0f5c233d1816fbec3
romainprignon <pro.rprignon@gmail.com>
2025-09-21 10:51:32 +0200

    bump ix

--- src/_internal/asynciterable/from.ts
+++ src/_internal/asynciterable/from.ts
@@ -3,7 +3,7 @@ import * as ix from 'ix/asynciterable'
 
 
 type From = <A extends UnknownArgs, R extends UnknownReturns>
-  (fun: Fun<A, ix.AsyncIterableInput<R>>) => (...args: A) => ix.AsyncIterableX<R>
+  (fun: Fun<A, ix.AsyncIterableX<R>>) => (...args: A) => ix.AsyncIterableX<R>
 const from: From = (fun) => (...args) =>
   ix.from(fun(...args))
 
