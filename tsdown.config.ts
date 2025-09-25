9971939b0bcf491fde123621035c6d3586105d60
romainprignon <pro.rprignon@gmail.com>
2025-09-25 08:54:33 +0200

    ✨ feat: improve build process by updating configuration files and adding bundle output

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,14 +3,13 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['./src/index.ts'],
   outDir: 'bundle/',
-  format: 'esm', // enlever de ts
-  // target: 'es5', // enlever d'ici, de ts car utilise packge.json, apres, c'est mieux d'etre explicite meme pour linter
+  format: 'esm',
   platform: 'neutral', // node and browser
   shims: true, // shims import.meta and require
   exports: true, // rewrite entrypoint in package.json
   treeshake: true,
-  dts: true, // enlever de ts
-  sourcemap: false, // result in a bigger bundle size than minified+sourcemap // reomve de ts
+  dts: true,
+  sourcemap: false, // result in a bigger bundle size than minified+sourcemap
   minify: false, // result in a bigger bundle size if used with sourcemap
   unbundle: false, // use true and minify false to debug,
   attw: true
