b19a20a7c2c9bc21feb0f367bb74e5bb0f3739eb
romainprignon <pro.rprignon@gmail.com>
2025-09-26 07:55:27 +0200

    ⬆️ chore: upgrade node version, use frozen-lockfile and npm publish

--- src/script/deploy.js
+++ src/script/deploy.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
 
-const shell = require('child_process').execSync
+import { execSync as shell } from 'child_process'
 
 const [_, otpVal] = process.argv.slice(2)
 
 shell('pinst --disable')
 shell('pnpm run bundle')
-shell(`pnpm publish --provenance --access public --otp=${otpVal}`)
+shell(`npm publish --provenance --access public --otp=${otpVal}`)
 shell('pinst --enable')
