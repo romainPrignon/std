ef45eac768982a7f9bd2e199a72e9f4513b28c08
romainprignon <pro.rprignon@gmail.com>
2026-05-06 14:59:23 +0200

    test(fp/arrays): improve test structure with AAA pattern

--- test/fp/arrays/index.test.ts
+++ test/fp/arrays/index.test.ts
@@ -1,10 +1,15 @@
-// test
-// import {  } from '../../../src/fp/arrays'
+import { describe, expect, it } from 'vitest'
 import * as arrays from '../../../src/fp/arrays/index.js'
 
-
 describe('arrays', () => {
-  it('should export arrays namespace', () => {
-    expect(Object.entries(arrays).length).toEqual(0)
+  it('should export empty namespace when no functions defined', () => {
+    // Arrange
+    const expectedExportCount = 0
+
+    // Act
+    const exportCount = Object.entries(arrays).length
+
+    // Assert
+    expect(exportCount).toEqual(expectedExportCount)
   })
 })
