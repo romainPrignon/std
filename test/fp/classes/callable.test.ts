800ddc08ea93a532461ba6e6c0ce3a84e350f878
romainprignon <pro.rprignon@gmail.com>
2026-05-06 21:28:28 +0200

    test(fp): improve classes/callable.test.ts according to guidelines

--- test/fp/classes/callable.test.ts
+++ test/fp/classes/callable.test.ts
@@ -6,11 +6,15 @@ import { callable } from '../../../src/fp/classes/callable.js'
 describe('fp/classes/callable.ts', () => {
   describe('callable()', () => {
     it('should be typed as a factory function', () => {
+      // Arrange
       class A {}
       const a = callable(A)
+
+      // Assert
       expectTypeOf<(...args: never[]) => A>(a)
       expectTypeOf<A>(a())
 
+      // Arrange
       class B {
         x: number
         y: string
@@ -21,16 +25,23 @@ describe('fp/classes/callable.ts', () => {
       }
 
       const b = callable(B)
+
+      // Assert
       expectTypeOf<(...args: [number, string]) => B>(b)
       expectTypeOf<B>(b(1, 'foo'))
     })
 
     it('should return a factory function', () => {
+      // Arrange
       class A { }
       const a = callable(A)
 
-      expect(a()).toBeInstanceOf(A)
-      expect(a()).toBeInstanceOf(a)
+      // Act
+      const result = a()
+
+      // Assert
+      expect(result).toBeInstanceOf(A)
+      expect(result).toBeInstanceOf(a)
     })
   })
 })
