c8bd561edf77061efd606cc605adb4230405bf32
romainprignon <pro.rprignon@gmail.com>
2026-05-06 15:27:05 +0200

    test(fp/objects): improve test with AAA pattern

--- test/fp/objects/index.test.ts
+++ test/fp/objects/index.test.ts
@@ -1,9 +1,15 @@
-// test
+import { describe, expect, it } from 'vitest'
 import * as objects from '../../../src/fp/objects/index.js'
 
-
 describe('objects', () => {
-  it('should export objects namespace', () => {
-    expect(Object.entries(objects).length).toEqual(0)
+  it('should export empty namespace when no functions defined', () => {
+    // Arrange
+    const expectedExportCount = 0
+
+    // Act
+    const exportCount = Object.entries(objects).length
+
+    // Assert
+    expect(exportCount).toEqual(expectedExportCount)
   })
 })
