ac8c3b94afe339b2aa79c13a7e026bf4dd153dde
romainprignon <pro.rprignon@gmail.com>
2026-05-06 21:37:51 +0200

    test(fp): improve errors/index.test.ts according to guidelines

--- test/fp/errors/index.test.ts
+++ test/fp/errors/index.test.ts
@@ -5,12 +5,20 @@ import * as errors from '../../../src/fp/errors/index.js'
 
 describe('errors', () => {
   it('should export errors namespace', () => {
-    expect(Object.entries(errors).length).toEqual(2)
+    // Arrange
+    const expectedLength = 2
+
+    // Act
+    const entries = Object.entries(errors)
+
+    // Assert
+    expect(entries.length).toEqual(expectedLength)
 
     expect(errors.Err).toBeDefined()
   })
 
   it('should export all namespace', () => {
+    // Assert
     expect(Err).toBeDefined()
   })
 })
