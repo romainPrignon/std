e99410f786b36b03063d896e2c6bb1274aa3b86c
romainprignon <pro.rprignon@gmail.com>
2026-05-06 21:25:30 +0200

    test(fp): improve arrays/index.test.ts according to guidelines

--- test/fp/arrays/index.test.ts
+++ test/fp/arrays/index.test.ts
@@ -5,6 +5,13 @@ import * as arrays from '../../../src/fp/arrays/index.js'
 
 describe('arrays', () => {
   it('should export arrays namespace', () => {
-    expect(Object.entries(arrays).length).toEqual(0)
+    // Arrange
+    const expectedLength = 0
+
+    // Act
+    const entries = Object.entries(arrays)
+
+    // Assert
+    expect(entries.length).toEqual(expectedLength)
   })
 })
