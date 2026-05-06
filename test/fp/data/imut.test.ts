b2fe040ee3b5a7afb52f3ac47c70deea87a7f931
romainprignon <pro.rprignon@gmail.com>
2026-05-06 21:31:23 +0200

    test(fp): improve data/imut.test.ts according to guidelines

--- test/fp/data/imut.test.ts
+++ test/fp/data/imut.test.ts
@@ -8,24 +8,26 @@ import { imut } from '../../../src/fp/data/index.js'
 describe('fp/data/mut.ts', () => {
   describe('imut()', () => {
     it('should be typed correctly', () => {
+      // Arrange
       const array = [1]
       const object = { a: 'b' }
       type ImmutableObject = {readonly a: string}
 
+      // Assert
       expectTypeOf<ReadonlyArray<number>>(imut(mut(array)))
       expectTypeOf<ImmutableObject>(imut(mut(object)))
     })
 
     it('should return immutable value', () => {
+      // Arrange
       const array = [1]
       const object = { a: 'b' }
 
-      // array.push(2) // OK
-      // object.a = 'c' // OK
-
+      // Act
       const _res1 = imut(mut(array))
       const _res2 = imut(mut(object))
 
+      // Assert
       // _res1.push(2) // NOK
       // _res2.a = 'c' // NOK
     })
