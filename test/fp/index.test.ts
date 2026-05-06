a7f21f719944d1eed2b0b54ad5341ff0e4673f3d
romainprignon <pro.rprignon@gmail.com>
2026-05-06 21:19:58 +0200

    Improve type coverage in test/fp/index.test.ts

--- test/fp/index.test.ts
+++ test/fp/index.test.ts
@@ -5,20 +5,21 @@ import * as fp from '../../src/fp/index.js'
 describe('fp', () => {
   describe('namespace exports', () => {
     it('should export fp namespace with all modules', () => {
-      // Arrange
-      const expectedModules = [
-        'arrays', 'classes', 'dates', 'errors', 'functions',
-        'numbers', 'objects', 'strings', 'data', 'struct'
-      ]
-
-      // Act
+      // Arrange & Act
       const moduleCount = Object.entries(fp).length
 
       // Assert
       expect(moduleCount).toEqual(10)
-      expectedModules.forEach(module => {
-        expect(fp[module as keyof typeof fp]).toBeDefined()
-      })
+      expect(fp.arrays).toBeDefined()
+      expect(fp.classes).toBeDefined()
+      expect(fp.dates).toBeDefined()
+      expect(fp.errors).toBeDefined()
+      expect(fp.functions).toBeDefined()
+      expect(fp.numbers).toBeDefined()
+      expect(fp.objects).toBeDefined()
+      expect(fp.strings).toBeDefined()
+      expect(fp.data).toBeDefined()
+      expect(fp.struct).toBeDefined()
     })
 
     it('should export all individual modules', () => {
