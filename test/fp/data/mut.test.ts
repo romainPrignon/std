7519e65b3b68dd7125df16abadeb167cd19c545b
romainprignon <pro.rprignon@gmail.com>
2026-05-06 21:33:05 +0200

    test(fp): improve data/mut.test.ts according to guidelines

--- test/fp/data/mut.test.ts
+++ test/fp/data/mut.test.ts
@@ -8,20 +8,25 @@ import { imut } from '../../../src/fp/data/index.js'
 describe('fp/data/mut.ts', () => {
   describe('mut()', () => {
     it('should be typed correctly', () => {
+      // Arrange
       const array = [1]
       const object = { a: 'b' }
 
+      // Assert
       expectTypeOf<typeof array>(mut(imut(array)))
       expectTypeOf<typeof object>(mut(imut(object)))
     })
 
     it('should return mutable value', () => {
+      // Arrange
       const array = [1]
       const object = { a: 'b' }
 
+      // Act
       const _res1 = mut(imut(array))
       const _res2 = mut(imut(object))
 
+      // Assert
       // _res1.push(2) // OK
       // _res2.a = 'c' // OK
     })
