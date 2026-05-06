f885a882262b5028482fb688ac2a317c8d4d194b
romainprignon <pro.rprignon@gmail.com>
2026-05-06 21:30:15 +0200

    test(fp): improve classes/instanceOf.test.ts according to guidelines

--- test/fp/classes/instanceOf.test.ts
+++ test/fp/classes/instanceOf.test.ts
@@ -7,34 +7,48 @@ import { instanceOf } from '../../../src/fp/classes/instanceOf.js'
 describe('fp/classes/instanceOf.ts', () => {
   describe('instanceOf()', () => {
     it('should be typed as boolean', () => {
+      // Arrange
       class A {}
       const a = new A()
 
+      // Assert
       expectTypeOf<boolean>(instanceOf(A, a))
     })
 
     it('should be typed as curried', () => {
+      // Arrange
       class A { }
       const a = new A()
       const instanceOfA = instanceOf(A)
 
+      // Assert
       expectTypeOf<(val: unknown) => boolean>(instanceOfA)
       expectTypeOf<boolean>(instanceOfA(a))
     })
 
     it('should return true if a instanceof A', () => {
+      // Arrange
       class A {}
       const a = new A()
 
-      expect(instanceOf(A, a)).toEqual(true)
+      // Act
+      const result = instanceOf(A, a)
+
+      // Assert
+      expect(result).toEqual(true)
     })
 
     it('should return false if b not instanceof A', () => {
+      // Arrange
       class A { }
       class B { }
       const b = new B()
 
-      expect(instanceOf(A, b)).toEqual(false)
+      // Act
+      const result = instanceOf(A, b)
+
+      // Assert
+      expect(result).toEqual(false)
     })
   })
 })
