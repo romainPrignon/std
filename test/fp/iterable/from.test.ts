4344e3ace42b115fa99ffb0135a9b521f0d6eb6f
romainprignon <pro.rprignon@gmail.com>
2026-05-06 15:18:01 +0200

    test(fp/iterable): improve tests with AAA pattern and better naming

--- test/fp/iterable/from.test.ts
+++ test/fp/iterable/from.test.ts
@@ -1,60 +1,111 @@
-import { expectTypeOf } from 'vitest'
+import { describe, expect, expectTypeOf, it } from 'vitest'
 import { Iterable } from 'ix'
 import ix from 'ix/iterable'
 import { map } from 'ix/iterable/operators'
-
-// test
 import { from } from '../../../src/fp/iterable/from.js'
-
 
 describe('fp/iterable/from.ts', () => {
   describe('from()', () => {
-    it('should be typed correctly for simple function', async () => {
+    it('should return typed function when wrapping simple function', () => {
+      // Act
       const fun = from(() => [1])
+
+      // Assert
       expectTypeOf<() => ix.IterableX<number>>(fun)
-
-      const res = fun()
-      expectTypeOf<ix.IterableX<number>>(res)
     })
 
-    it('should be typed correctly for function returning parameter', () => {
-      const fun = from((a) => [a])
-      expectTypeOf<(...args: Array<unknown>) => ix.IterableX<unknown>>(fun)
-
-      const res = fun(1)
-      expectTypeOf<ix.IterableX<unknown>>(res)
-    })
-
-    it('should be typed correctly for function with typed params', () => {
-      const fun = from((a: number) => [a])
-      expectTypeOf<(...args: Array<number>) => ix.IterableX<number>>(fun)
-
-      const res = fun(1)
-      expectTypeOf<ix.IterableX<number>>(res)
-    })
-
-    it('should be typed correctly for function with multiple params', () => {
-      const fun = from((a: number, b: string) => a + b)
-      expectTypeOf<(...args: [number, string]) => ix.IterableX<number | string>>(fun)
-
-      const res = fun(1, 'a')
-      expectTypeOf<ix.IterableX<number | string>>(res)
-    })
-
-    it('should make result async iterable from fun', async () => {
+    it('should return typed iterable when calling wrapped function', () => {
       // Arrange
       const fun = from(() => [1])
 
       // Act
-      expect(fun()).toBeInstanceOf(Iterable)
+      const res = fun()
 
       // Assert
-      fun().pipe(
-        map(val => val + 1)
-      ).forEach(val => expect(val).toEqual(2))
+      expectTypeOf<ix.IterableX<number>>(res)
     })
 
-    it('should make result iterable from generator', async () => {
+    it('should infer unknown type when function returns parameter without type', () => {
+      // Act
+      const fun = from((a) => [a])
+
+      // Assert
+      expectTypeOf<(...args: Array<unknown>) => ix.IterableX<unknown>>(fun)
+    })
+
+    it('should return unknown iterable when calling untyped function', () => {
+      // Arrange
+      const fun = from((a) => [a])
+
+      // Act
+      const res = fun(1)
+
+      // Assert
+      expectTypeOf<ix.IterableX<unknown>>(res)
+    })
+
+    it('should preserve parameter type when function has typed params', () => {
+      // Act
+      const fun = from((a: number) => [a])
+
+      // Assert
+      expectTypeOf<(...args: Array<number>) => ix.IterableX<number>>(fun)
+    })
+
+    it('should return typed iterable when calling typed function', () => {
+      // Arrange
+      const fun = from((a: number) => [a])
+
+      // Act
+      const res = fun(1)
+
+      // Assert
+      expectTypeOf<ix.IterableX<number>>(res)
+    })
+
+    it('should preserve tuple type when function has multiple params', () => {
+      // Act
+      const fun = from((a: number, b: string) => a + b)
+
+      // Assert
+      expectTypeOf<(...args: [number, string]) => ix.IterableX<number | string>>(fun)
+    })
+
+    it('should return union typed iterable when calling multi-param function', () => {
+      // Arrange
+      const fun = from((a: number, b: string) => a + b)
+
+      // Act
+      const res = fun(1, 'a')
+
+      // Assert
+      expectTypeOf<ix.IterableX<number | string>>(res)
+    })
+
+    it('should create iterable instance when wrapping function', () => {
+      // Arrange
+      const fun = from(() => [1])
+
+      // Act
+      const result = fun()
+
+      // Assert
+      expect(result).toBeInstanceOf(Iterable)
+    })
+
+    it('should map values correctly when piping operations', () => {
+      // Arrange
+      const fun = from(() => [1])
+      const expectedValue = 2
+
+      // Act
+      const result = fun().pipe(map(val => val + 1))
+
+      // Assert
+      result.forEach(val => expect(val).toEqual(expectedValue))
+    })
+
+    it('should create iterable from generator function', () => {
       // Arrange
       const source = function * (): Generator<number> {
         yield 1
@@ -62,12 +113,25 @@ describe('fp/iterable/from.ts', () => {
 
       // Act
       const fun:() => Iterable<number> = from(source)
+      const result = fun()
 
       // Assert
-      expect(fun()).toBeInstanceOf(Iterable)
-      fun().pipe(
-        map(val => val + 1)
-      ).forEach(val => expect(val).toEqual(2))
+      expect(result).toBeInstanceOf(Iterable)
+    })
+
+    it('should map values from generator when piping operations', () => {
+      // Arrange
+      const source = function * (): Generator<number> {
+        yield 1
+      }
+      const fun = from(source)
+      const expectedValue = 2
+
+      // Act
+      const result = fun().pipe(map(val => val + 1))
+
+      // Assert
+      result.forEach(val => expect(val).toEqual(expectedValue))
     })
   })
 })
--- test/fp/iterable/index.test.ts
+++ test/fp/iterable/index.test.ts
@@ -1,16 +1,26 @@
-// test
+import { describe, expect, it } from 'vitest'
 import { from } from '../../../src/fp/iterable/index.js'
 import * as iterable from '../../../src/fp/iterable/index.js'
 
-
 describe('iterable', () => {
-  it('should export iterable namespace', () => {
-    expect(Object.entries(iterable).length).toEqual(1)
+  it('should export one function when from is defined', () => {
+    // Arrange
+    const expectedExportCount = 1
 
+    // Act
+    const exportCount = Object.entries(iterable).length
+
+    // Assert
+    expect(exportCount).toEqual(expectedExportCount)
+  })
+
+  it('should export from function from namespace', () => {
+    // Act & Assert
     expect(iterable.from).toBeDefined()
   })
 
-  it('should export all namespace', () => {
+  it('should export from function as named export', () => {
+    // Act & Assert
     expect(from).toBeDefined()
   })
 })
