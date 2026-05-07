4c2148bee50e42e4206fe57425f4fcfe4ac26178
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:14:40 +0200

    test(fp/numbers): improve number.test.ts following guidelines

--- test/fp/numbers/number.test.ts
+++ test/fp/numbers/number.test.ts
@@ -1,21 +1,33 @@
+import { describe, expect, it } from 'vitest'
 // test
 import { isMultipleOf } from '../../../src/fp/numbers/number.js'
-
 
 describe('fp/numbers/number.ts', () => {
   describe('isMultipleOf()', () => {
     it('should return true for 15 multiple of 5', () => {
+      // Arrange
       const isMultipleOf5 = isMultipleOf(5)
 
-      expect(isMultipleOf(5, 15)).toEqual(true)
-      expect(isMultipleOf5(15)).toEqual(true)
+      // Act
+      const result1 = isMultipleOf(5, 15)
+      const result2 = isMultipleOf5(15)
+
+      // Assert
+      expect(result1).toEqual(true)
+      expect(result2).toEqual(true)
     })
 
     it('should return false for 16 multiple of 5', () => {
+      // Arrange
       const isMultipleOf5 = isMultipleOf(5)
 
-      expect(isMultipleOf(5, 16)).toEqual(false)
-      expect(isMultipleOf5(16)).toEqual(false)
+      // Act
+      const result1 = isMultipleOf(5, 16)
+      const result2 = isMultipleOf5(16)
+
+      // Assert
+      expect(result1).toEqual(false)
+      expect(result2).toEqual(false)
     })
   })
 })
