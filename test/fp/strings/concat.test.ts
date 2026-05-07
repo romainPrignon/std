b5bc8624860d74545f11df7c1a13eb94a6eb1073
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:15:22 +0200

    test(fp/strings): improve concat.test.ts following guidelines

--- test/fp/strings/concat.test.ts
+++ test/fp/strings/concat.test.ts
@@ -1,22 +1,28 @@
-import { expectTypeOf } from 'vitest'
+import { describe, expect, it, expectTypeOf } from 'vitest'
 
 // test
 import { concat } from '../../../src/fp/strings/concat.js'
 
-
 describe('fp/strings/concat.ts', () => {
   describe('concat()', () => {
     it('should be typed as curried', () => {
+      // Assert
       expectTypeOf<(str: string) => string>(concat('a'))
     })
 
     it('should be typed as string', () => {
+      // Assert
       expectTypeOf<string>(concat('a', 'b'))
     })
 
     it('should return a concat string', () => {
-      expect(concat('b', 'a')).toEqual('ab')
-      expect(concat('b')('a')).toEqual('ab')
+      // Act
+      const result1 = concat('b', 'a')
+      const result2 = concat('b')('a')
+
+      // Assert
+      expect(result1).toEqual('ab')
+      expect(result2).toEqual('ab')
     })
   })
 })
