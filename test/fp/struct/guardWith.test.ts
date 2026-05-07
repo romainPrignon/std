b0cd87b437e2db71e38ca636663d6f442c11fcef
romainprignon <pro.rprignon@gmail.com>
2026-05-07 12:22:04 +0200

    test(fp): improve struct/guardWith.test.ts according to guidelines

--- test/fp/struct/guardWith.test.ts
+++ test/fp/struct/guardWith.test.ts
@@ -8,36 +8,53 @@ import { guardWith } from '../../../src/oop/struct/guardWith.js'
 describe('oop/struct/guardWith.ts', () => {
   describe('guardWith()', () => {
     it('should be typed correctly', () => {
+      // Arrange
       const numberSchema = z.number()
+
+      // Act
       const numberGuard = guardWith(numberSchema)
 
+      // Assert
       expectTypeOf<(input: unknown) => boolean>(numberGuard)
     })
+
     it('should be typed correctly when calling guard', () => {
+      // Arrange
       const numberSchema = z.number()
       const input = {}
 
+      // Act
       const numberGuard = guardWith(numberSchema)
+      const result = numberGuard(input)
 
-      expectTypeOf<boolean>(numberGuard(input))
+      // Assert
+      expectTypeOf<boolean>(result)
     })
 
     it('should return false if input is not a number', () => {
+      // Arrange
       const numberSchema = z.number()
       const input = {}
-
       const numberGuard = guardWith(numberSchema)
 
-      expect(numberGuard(input)).toEqual(false)
+      // Act
+      const result = numberGuard(input)
+
+      // Assert
+      expect(result).toEqual(false)
     })
 
     it('should return true if input is a number', () => {
+      // Arrange
       const numberSchema = z.number()
       const input = 1
-
       const numberGuard = guardWith(numberSchema)
 
-      expect(numberGuard(input)).toEqual(true)
+      // Act
+      const result = numberGuard(input)
+
+      // Assert
+      expect(result).toEqual(true)
     })
   })
 })
