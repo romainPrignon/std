99170bdc0e4d7bd6a075b0e1f9a91678a7865849
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:12:53 +0200

    test(fp/functions): improve maybe.test.ts following guidelines

--- test/fp/functions/maybe.test.ts
+++ test/fp/functions/maybe.test.ts
@@ -1,15 +1,16 @@
+import { describe, expect, it, expectTypeOf } from 'vitest'
 import { type Maybe } from '../../../src/type/index.js'
-
-import { expectTypeOf } from 'vitest'
 
 // test
 import { maybe } from '../../../src/fp/functions/maybe.js'
 
-
 describe('fp/functions/maybe.ts', () => {
   describe('maybe()', () => {
     it('should be typed correctly for simple function', () => {
+      // Arrange
       const opt = maybe(() => 1)
+
+      // Assert
       expectTypeOf<(...args: Array<never>) => Maybe<number>>(opt)
 
       const res = opt()
@@ -17,7 +18,10 @@ describe('fp/functions/maybe.ts', () => {
     })
 
     it('should be typed correctly for function returning undefined', () => {
+      // Arrange
       const opt = maybe(() => undefined)
+
+      // Assert
       expectTypeOf<(...args: Array<never>) => Maybe<undefined>>(opt)
 
       const res = opt()
@@ -25,7 +29,10 @@ describe('fp/functions/maybe.ts', () => {
     })
 
     it('should be typed correctly for function returning parameter', () => {
+      // Arrange
       const opt = maybe((a) => a)
+
+      // Assert
       expectTypeOf<(...args: Array<unknown>) => Maybe<unknown>>(opt)
 
       const res = opt(1)
@@ -33,7 +40,10 @@ describe('fp/functions/maybe.ts', () => {
     })
 
     it('should be typed correctly for function with typed params', () => {
+      // Arrange
       const opt = maybe((a: number) => a)
+
+      // Assert
       expectTypeOf<(...args: Array<number>) => Maybe<number>>(opt)
 
       const res = opt(1)
@@ -41,7 +51,10 @@ describe('fp/functions/maybe.ts', () => {
     })
 
     it('should be typed correctly for function with multiple params', () => {
+      // Arrange
       const opt = maybe((a: number, b: string) => a + b)
+
+      // Assert
       expectTypeOf<(...args: [number, string]) => Maybe<number | string>>(opt)
 
       const res = opt(1, 'a')
@@ -49,27 +62,47 @@ describe('fp/functions/maybe.ts', () => {
     })
 
     it('should make result maybe', () => {
+      // Arrange
       const opt = maybe(() => 1)
 
-      expect(opt()).toEqual(1)
+      // Act
+      const result = opt()
+
+      // Assert
+      expect(result).toEqual(1)
     })
 
     it('should not change undefined result', () => {
+      // Arrange
       const opt = maybe(() => undefined)
 
-      expect(opt()).toEqual(undefined)
+      // Act
+      const result = opt()
+
+      // Assert
+      expect(result).toEqual(undefined)
     })
 
     it('should not change input', () => {
+      // Arrange
       const opt = maybe((a) => a)
 
-      expect(opt(1)).toEqual(1)
+      // Act
+      const result = opt(1)
+
+      // Assert
+      expect(result).toEqual(1)
     })
 
     it('should not change inputs', () => {
+      // Arrange
       const opt = maybe((a: number, b: number) => a + b)
 
-      expect(opt(1, 1)).toEqual(2)
+      // Act
+      const result = opt(1, 1)
+
+      // Assert
+      expect(result).toEqual(2)
     })
   })
 })
