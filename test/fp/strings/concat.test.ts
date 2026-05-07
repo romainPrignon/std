9f21010a43ecaf04da823085989bd32705884360
romainprignon <pro.rprignon@gmail.com>
2026-05-07 12:17:47 +0200

    test(fp): improve strings/concat.test.ts according to guidelines

--- test/fp/strings/concat.test.ts
+++ test/fp/strings/concat.test.ts
@@ -7,16 +7,33 @@ import { concat } from '../../../src/fp/strings/concat.js'
 describe('fp/strings/concat.ts', () => {
   describe('concat()', () => {
     it('should be typed as curried', () => {
-      expectTypeOf<(str: string) => string>(concat('a'))
+      // Act
+      const result = concat('a')
+
+      // Assert
+      expectTypeOf<(str: string) => string>(result)
     })
 
     it('should be typed as string', () => {
-      expectTypeOf<string>(concat('a', 'b'))
+      // Act
+      const result = concat('a', 'b')
+
+      // Assert
+      expectTypeOf<string>(result)
     })
 
     it('should return a concat string', () => {
-      expect(concat('b', 'a')).toEqual('ab')
-      expect(concat('b')('a')).toEqual('ab')
+      // Arrange
+      const str1 = 'b'
+      const str2 = 'a'
+
+      // Act
+      const res1 = concat(str1, str2)
+      const res2 = concat(str1)(str2)
+
+      // Assert
+      expect(res1).toEqual('ab')
+      expect(res2).toEqual('ab')
     })
   })
 })
