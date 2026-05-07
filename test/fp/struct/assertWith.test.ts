6273a2e1fc9ddcff8968873d99b0a14a7f65d055
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:16:23 +0200

    test(fp/struct): improve assertWith.test.ts following guidelines

--- test/fp/struct/assertWith.test.ts
+++ test/fp/struct/assertWith.test.ts
@@ -1,42 +1,47 @@
-import { expectTypeOf } from 'vitest'
+import { describe, expect, it, expectTypeOf } from 'vitest'
 import * as z from 'zod'
 
 // test
 import { assertWith } from '../../../src/oop/struct/assertWith.js'
 
-
 describe('oop/struct/assertWith.ts', () => {
   describe('assertWith()', () => {
     it('should be typed correctly', () => {
+      // Arrange
       const numberSchema = z.number()
       const numberAssertion = assertWith(numberSchema)
 
+      // Assert
       expectTypeOf<(input: unknown) => void>(numberAssertion)
     })
+
     it('should be typed correctly when calling assertion', () => {
+      // Arrange
       const numberSchema = z.number()
       const input = 1
-
       const numberAssertion = assertWith(numberSchema)
 
+      // Assert
       expectTypeOf<void>(numberAssertion(input))
     })
 
-    it('should return false if input is not a number', () => {
+    it('should throw if input is not a number', () => {
+      // Arrange
       const numberSchema = z.number()
       const input = {}
-
       const numberAssertion = assertWith(numberSchema)
 
+      // Act & Assert
       expect(() => numberAssertion(input)).toThrow()
     })
 
-    it('should return true if input is a number', () => {
+    it('should not throw if input is a number', () => {
+      // Arrange
       const numberSchema = z.number()
       const input = 1
-
       const numberAssertion = assertWith(numberSchema)
 
+      // Act & Assert
       expect(() => numberAssertion(input)).not.toThrow()
     })
   })
