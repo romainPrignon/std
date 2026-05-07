a996f262dddd4dcec704a7c615bcb1dee251313f
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:11:43 +0200

    test(fp/functions): improve index.test.ts following guidelines

--- test/fp/functions/index.test.ts
+++ test/fp/functions/index.test.ts
@@ -1,22 +1,27 @@
-// test
+import { describe, expect, it } from 'vitest'
 import { may, mayAsync, maybe, raise } from '../../../src/fp/functions/index.js'
 import * as functions from '../../../src/fp/functions/index.js'
 
-
 describe('functions', () => {
-  it('should export functions namespace', () => {
-    expect(Object.entries(functions).length).toEqual(4)
+  describe('namespace', () => {
+    it('should export functions namespace with 4 entries', () => {
+      // Act
+      const entries = Object.entries(functions)
 
-    expect(functions.may).toBeDefined()
-    expect(functions.mayAsync).toBeDefined()
-    expect(functions.maybe).toBeDefined()
-    expect(functions.raise).toBeDefined()
-  })
+      // Assert
+      expect(entries.length).toEqual(4)
+      expect(functions.may).toBeDefined()
+      expect(functions.mayAsync).toBeDefined()
+      expect(functions.maybe).toBeDefined()
+      expect(functions.raise).toBeDefined()
+    })
 
-  it('should export all namespace', () => {
-    expect(may).toBeDefined()
-    expect(mayAsync).toBeDefined()
-    expect(maybe).toBeDefined()
-    expect(raise).toBeDefined()
+    it('should export all individual functions', () => {
+      // Assert
+      expect(may).toBeDefined()
+      expect(mayAsync).toBeDefined()
+      expect(maybe).toBeDefined()
+      expect(raise).toBeDefined()
+    })
   })
 })
