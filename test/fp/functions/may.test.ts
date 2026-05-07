b8690f7b25cc8e55c250fc45618c56bdce77433d
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:12:05 +0200

    test(fp/functions): improve may.test.ts following guidelines

--- test/fp/functions/may.test.ts
+++ test/fp/functions/may.test.ts
@@ -1,42 +1,52 @@
-import { expectTypeOf } from 'vitest'
+import { describe, expect, it, expectTypeOf } from 'vitest'
 import { Err } from '../../../src/fp/errors/Error.js'
 
 // test
 import { may } from '../../../src/fp/functions/may.js'
 
-
 describe('fp/functions/may.ts', () => {
   describe('may()', () => {
     it('should be typed as R1', () => {
+      // Assert
       expectTypeOf<number>(may(() => 1))
     })
+
     it('should be typed as R1 | R2', () => {
+      // Assert
       expectTypeOf<number | string>(may(() => 1, () => 'a'))
     })
 
     it('should return an Err if failure callback not provided', () => {
+      // Arrange
       const err = Err('boom', { code: 'BOOM' })
 
-      expect(
-        may(() => { throw err })
-      ).toEqual(err)
+      // Act
+      const result = may(() => { throw err })
+
+      // Assert
+      expect(result).toEqual(err)
     })
 
     it('should return correctly in success case', () => {
-      expect(
-        may(() => 1)
-      ).toEqual(1)
+      // Act
+      const result = may(() => 1)
+
+      // Assert
+      expect(result).toEqual(1)
     })
 
     it('should return fallback in failure case', () => {
+      // Arrange
       const err = Err('boom', { code: 'BOOM' })
 
-      expect(
-        may(
-          () => { throw err },
-          () => 2
-        )
-      ).toEqual(2)
+      // Act
+      const result = may(
+        () => { throw err },
+        () => 2
+      )
+
+      // Assert
+      expect(result).toEqual(2)
     })
   })
 })
