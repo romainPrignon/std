a5d494974fb923a71b6ef108f7ab47ea25990b76
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:05:48 +0200

    test(fp/dates): improve index.test.ts following guidelines

--- test/fp/dates/index.test.ts
+++ test/fp/dates/index.test.ts
@@ -1,20 +1,25 @@
-// test
+import { describe, expect, it } from 'vitest'
 import { date, epoch, now } from '../../../src/fp/dates/index.js'
 import * as dates from '../../../src/fp/dates/index.js'
 
-
 describe('dates', () => {
-  it('should export dates namespace', () => {
-    expect(Object.entries(dates).length).toEqual(3)
+  describe('namespace', () => {
+    it('should export dates namespace with 3 entries', () => {
+      // Act
+      const entries = Object.entries(dates)
 
-    expect(dates.date).toBeDefined()
-    expect(dates.epoch).toBeDefined()
-    expect(dates.now).toBeDefined()
-  })
+      // Assert
+      expect(entries.length).toEqual(3)
+      expect(dates.date).toBeDefined()
+      expect(dates.epoch).toBeDefined()
+      expect(dates.now).toBeDefined()
+    })
 
-  it('should export all namespace', () => {
-    expect(date).toBeDefined()
-    expect(epoch).toBeDefined()
-    expect(now).toBeDefined()
+    it('should export all individual functions', () => {
+      // Assert
+      expect(date).toBeDefined()
+      expect(epoch).toBeDefined()
+      expect(now).toBeDefined()
+    })
   })
 })
