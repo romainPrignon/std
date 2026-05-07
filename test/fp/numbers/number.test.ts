bfc03ab5902ca39a57d956147caad3fd90915179
romainprignon <pro.rprignon@gmail.com>
2026-05-07 12:15:21 +0200

    test(fp): improve numbers/number.test.ts according to guidelines

--- test/fp/numbers/number.test.ts
+++ test/fp/numbers/number.test.ts
@@ -5,17 +5,29 @@ import { isMultipleOf } from '../../../src/fp/numbers/number.js'
 describe('fp/numbers/number.ts', () => {
   describe('isMultipleOf()', () => {
     it('should return true for 15 multiple of 5', () => {
+      // Arrange
       const isMultipleOf5 = isMultipleOf(5)
 
-      expect(isMultipleOf(5, 15)).toEqual(true)
-      expect(isMultipleOf5(15)).toEqual(true)
+      // Act
+      const res1 = isMultipleOf(5, 15)
+      const res2 = isMultipleOf5(15)
+
+      // Assert
+      expect(res1).toEqual(true)
+      expect(res2).toEqual(true)
     })
 
     it('should return false for 16 multiple of 5', () => {
+      // Arrange
       const isMultipleOf5 = isMultipleOf(5)
 
-      expect(isMultipleOf(5, 16)).toEqual(false)
-      expect(isMultipleOf5(16)).toEqual(false)
+      // Act
+      const res1 = isMultipleOf(5, 16)
+      const res2 = isMultipleOf5(16)
+
+      // Assert
+      expect(res1).toEqual(false)
+      expect(res2).toEqual(false)
     })
   })
 })
