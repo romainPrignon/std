1c3c9bd399e8980cc52658189c431dc42def4b85
romainprignon <pro.rprignon@gmail.com>
2026-05-07 12:16:33 +0200

    test(fp): improve objects/index.test.ts according to guidelines

--- test/fp/objects/index.test.ts
+++ test/fp/objects/index.test.ts
@@ -4,6 +4,13 @@ import * as objects from '../../../src/fp/objects/index.js'
 
 describe('objects', () => {
   it('should export objects namespace', () => {
-    expect(Object.entries(objects).length).toEqual(0)
+    // Arrange
+    const expectedLength = 0
+
+    // Act
+    const entries = Object.entries(objects)
+
+    // Assert
+    expect(entries.length).toEqual(expectedLength)
   })
 })
