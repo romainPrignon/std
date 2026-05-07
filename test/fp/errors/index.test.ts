3fa85d5070ff374a73f49d0b11973d47f8ab0e8d
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:11:21 +0200

    test(fp/errors): improve index.test.ts following guidelines

--- test/fp/errors/index.test.ts
+++ test/fp/errors/index.test.ts
@@ -1,16 +1,23 @@
-// test
-import { Err } from '../../../src/fp/errors/index.js'
+import { describe, expect, it } from 'vitest'
+import { Err, inherit } from '../../../src/fp/errors/index.js'
 import * as errors from '../../../src/fp/errors/index.js'
 
-
 describe('errors', () => {
-  it('should export errors namespace', () => {
-    expect(Object.entries(errors).length).toEqual(2)
+  describe('namespace', () => {
+    it('should export errors namespace with 2 entries', () => {
+      // Act
+      const entries = Object.entries(errors)
 
-    expect(errors.Err).toBeDefined()
-  })
+      // Assert
+      expect(entries.length).toEqual(2)
+      expect(errors.Err).toBeDefined()
+      expect(errors.inherit).toBeDefined()
+    })
 
-  it('should export all namespace', () => {
-    expect(Err).toBeDefined()
+    it('should export all individual functions', () => {
+      // Assert
+      expect(Err).toBeDefined()
+      expect(inherit).toBeDefined()
+    })
   })
 })
