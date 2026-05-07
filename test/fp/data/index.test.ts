41555fd20b943192e85cecd4edacc4125395be82
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:05:07 +0200

    test(fp/data): improve index.test.ts following guidelines

--- test/fp/data/index.test.ts
+++ test/fp/data/index.test.ts
@@ -1,18 +1,23 @@
-// test
+import { describe, expect, it } from 'vitest'
 import { imut, mut } from '../../../src/fp/data/index.js'
 import * as data from '../../../src/fp/data/index.js'
 
-
 describe('data', () => {
-  it('should export data namespace', () => {
-    expect(Object.entries(data).length).toEqual(2)
+  describe('namespace', () => {
+    it('should export data namespace with 2 entries', () => {
+      // Act
+      const entries = Object.entries(data)
 
-    expect(data.imut).toBeDefined()
-    expect(data.mut).toBeDefined()
-  })
+      // Assert
+      expect(entries.length).toEqual(2)
+      expect(data.imut).toBeDefined()
+      expect(data.mut).toBeDefined()
+    })
 
-  it('should export all namespace', () => {
-    expect(imut).toBeDefined()
-    expect(mut).toBeDefined()
+    it('should export all individual functions', () => {
+      // Assert
+      expect(imut).toBeDefined()
+      expect(mut).toBeDefined()
+    })
   })
 })
