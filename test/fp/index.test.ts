877b1f870ac17bdfbce1a8bfca1c7b8219fcd865
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:02:09 +0200

    test(fp): improve index.test.ts following guidelines

--- test/fp/index.test.ts
+++ test/fp/index.test.ts
@@ -1,34 +1,39 @@
-// test
+import { describe, expect, it } from 'vitest'
 import { arrays, classes, dates, errors, functions, numbers, objects, strings, data, struct } from '../../src/fp/index.js'
 import * as fp from '../../src/fp/index.js'
 
-
 describe('fp', () => {
-  it('should export fp namespace', () => {
-    expect(Object.entries(fp).length).toEqual(10)
+  describe('namespace', () => {
+    it('should export fp namespace with 10 entries', () => {
+      // Act
+      const entries = Object.entries(fp)
 
-    expect(fp.arrays).toBeDefined()
-    expect(fp.classes).toBeDefined()
-    expect(fp.dates).toBeDefined()
-    expect(fp.errors).toBeDefined()
-    expect(fp.functions).toBeDefined()
-    expect(fp.numbers).toBeDefined()
-    expect(fp.objects).toBeDefined()
-    expect(fp.strings).toBeDefined()
-    expect(fp.data).toBeDefined()
-    expect(fp.struct).toBeDefined()
-  })
+      // Assert
+      expect(entries.length).toEqual(10)
+      expect(fp.arrays).toBeDefined()
+      expect(fp.classes).toBeDefined()
+      expect(fp.dates).toBeDefined()
+      expect(fp.errors).toBeDefined()
+      expect(fp.functions).toBeDefined()
+      expect(fp.numbers).toBeDefined()
+      expect(fp.objects).toBeDefined()
+      expect(fp.strings).toBeDefined()
+      expect(fp.data).toBeDefined()
+      expect(fp.struct).toBeDefined()
+    })
 
-  it('should export all namespace', () => {
-    expect(arrays).toBeDefined()
-    expect(classes).toBeDefined()
-    expect(dates).toBeDefined()
-    expect(errors).toBeDefined()
-    expect(functions).toBeDefined()
-    expect(numbers).toBeDefined()
-    expect(objects).toBeDefined()
-    expect(strings).toBeDefined()
-    expect(data).toBeDefined()
-    expect(struct).toBeDefined()
+    it('should export all individual namespaces', () => {
+      // Assert
+      expect(arrays).toBeDefined()
+      expect(classes).toBeDefined()
+      expect(dates).toBeDefined()
+      expect(errors).toBeDefined()
+      expect(functions).toBeDefined()
+      expect(numbers).toBeDefined()
+      expect(objects).toBeDefined()
+      expect(strings).toBeDefined()
+      expect(data).toBeDefined()
+      expect(struct).toBeDefined()
+    })
   })
 })
