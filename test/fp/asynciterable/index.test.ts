818cd4d7bdfd0ed2c6fa5f409a17a9366c985ce3
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:03:25 +0200

    test(fp/asynciterable): improve index.test.ts following guidelines

--- test/fp/asynciterable/index.test.ts
+++ test/fp/asynciterable/index.test.ts
@@ -1,16 +1,21 @@
-// test
+import { describe, expect, it } from 'vitest'
 import { from } from '../../../src/fp/asynciterable/index.js'
 import * as asynciterable from '../../../src/fp/asynciterable/index.js'
 
-
 describe('asynciterable', () => {
-  it('should export asynciterable namespace', () => {
-    expect(Object.entries(asynciterable).length).toEqual(1)
+  describe('namespace', () => {
+    it('should export asynciterable namespace with 1 entry', () => {
+      // Act
+      const entries = Object.entries(asynciterable)
 
-    expect(asynciterable.from).toBeDefined()
-  })
+      // Assert
+      expect(entries.length).toEqual(1)
+      expect(asynciterable.from).toBeDefined()
+    })
 
-  it('should export all namespace', () => {
-    expect(from).toBeDefined()
+    it('should export all individual functions', () => {
+      // Assert
+      expect(from).toBeDefined()
+    })
   })
 })
