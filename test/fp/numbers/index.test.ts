d44c0a661242f2c80a3d523cfa6a1c0c19527efc
romainprignon <pro.rprignon@gmail.com>
2026-05-07 12:10:48 +0200

    test(fp): improve numbers/index.test.ts according to guidelines

--- test/fp/numbers/index.test.ts
+++ test/fp/numbers/index.test.ts
@@ -5,12 +5,20 @@ import * as numbers from '../../../src/fp/numbers/index.js'
 
 describe('numbers', () => {
   it('should export numbers namespace', () => {
-    expect(Object.entries(numbers).length).toEqual(1)
+    // Arrange
+    const expectedLength = 1
+
+    // Act
+    const entries = Object.entries(numbers)
+
+    // Assert
+    expect(entries.length).toEqual(expectedLength)
 
     expect(numbers.isMultipleOf).toBeDefined()
   })
 
   it('should export all namespace', () => {
+    // Assert
     expect(isMultipleOf).toBeDefined()
   })
 })
