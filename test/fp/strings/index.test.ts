ae815c4e1be9bf38a8a1b110918a614b3285df6c
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:15:42 +0200

    test(fp/strings): improve index.test.ts following guidelines

--- test/fp/strings/index.test.ts
+++ test/fp/strings/index.test.ts
@@ -1,16 +1,21 @@
-// test
+import { describe, expect, it } from 'vitest'
 import { concat } from '../../../src/fp/strings/index.js'
 import * as strings from '../../../src/fp/strings/index.js'
 
-
 describe('strings', () => {
-  it('should export strings namespace', () => {
-    expect(Object.entries(strings).length).toEqual(1)
+  describe('namespace', () => {
+    it('should export strings namespace with 1 entry', () => {
+      // Act
+      const entries = Object.entries(strings)
 
-    expect(strings.concat).toBeDefined()
-  })
+      // Assert
+      expect(entries.length).toEqual(1)
+      expect(strings.concat).toBeDefined()
+    })
 
-  it('should export all namespace', () => {
-    expect(concat).toBeDefined()
+    it('should export all individual functions', () => {
+      // Assert
+      expect(concat).toBeDefined()
+    })
   })
 })
