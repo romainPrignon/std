b488016038955b9a01fbef83792a7ec68dae0c84
romainprignon <pro.rprignon@gmail.com>
2026-05-07 14:04:06 +0200

    test(fp/classes): improve index.test.ts following guidelines

--- test/fp/classes/index.test.ts
+++ test/fp/classes/index.test.ts
@@ -1,18 +1,23 @@
-// test
+import { describe, expect, it } from 'vitest'
 import { callable, instanceOf } from '../../../src/fp/classes/index.js'
 import * as classes from '../../../src/fp/classes/index.js'
 
-
 describe('classes', () => {
-  it('should export classes namespace', () => {
-    expect(Object.entries(classes).length).toEqual(2)
+  describe('namespace', () => {
+    it('should export classes namespace with 2 entries', () => {
+      // Act
+      const entries = Object.entries(classes)
 
-    expect(classes.callable).toBeDefined()
-    expect(classes.instanceOf).toBeDefined()
-  })
+      // Assert
+      expect(entries.length).toEqual(2)
+      expect(classes.callable).toBeDefined()
+      expect(classes.instanceOf).toBeDefined()
+    })
 
-  it('should export all namespace', () => {
-    expect(callable).toBeDefined()
-    expect(instanceOf).toBeDefined()
+    it('should export all individual functions', () => {
+      // Assert
+      expect(callable).toBeDefined()
+      expect(instanceOf).toBeDefined()
+    })
   })
 })
